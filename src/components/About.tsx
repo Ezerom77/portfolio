c0728b6a983a180d42a9fd61bf0909f50734c59f
Ezerom77 <fullstack@ezerom.com.ar>
2023-10-20 09:02:51 -0300

    about adjusted

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,7 +20,7 @@ const About = ({ pageInfo }: Props) => {
       }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-16 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
 
@@ -38,16 +38,16 @@ const About = ({ pageInfo }: Props) => {
         }}
         viewport={{ once: true }}
         src={urlForImage(pageInfo?.profilePic).url()}
-        className=" -mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover
+        className=" -mb-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover
         md:rounded-lg md:w-64 md:h95 xl:w-[500px] xl:h-[600px]"
       />
-      <div className=" space-y-10 px-0 md:px-10">
-        <h4 className=" text-4xl font-semibold">
+      <div className=" space-y-5 md:space-y-10 px-0 md:px-10">
+        <h4 className=" text-lg md:text-4xl font-semibold">
           Here is a{" "}
           <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
           background
         </h4>
-        <p className="text-base">{pageInfo?.backgroudInformation}</p>
+        <p className="text-xs md:text-base">{pageInfo?.backgroudInformation}</p>
       </div>
     </motion.div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,7 @@ const Projects = ({ projects }: Props) => {
       }}
       className="h-screen relative flex overflow-hidden  flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-16 md:top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
@@ -31,7 +31,7 @@ const Projects = ({ projects }: Props) => {
         {projects.map((project, i) => (
           <div
             key={project._id}
-            className=" w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
+            className=" w-screen flex-shrink-0 snap-center flex flex-col space-y-2 md:space-y-5 items-center justify-center p-3 md:p-44 h-screen"
           >
             <Link href={project.linkToBuild} target="_blank">
               <motion.img
@@ -49,18 +49,18 @@ const Projects = ({ projects }: Props) => {
                 viewport={{ once: true }}
                 src={urlForImage(project?.image).url()}
                 alt=""
-                className="h-[300px]  rounded shadow-2xl"
+                className="h-[80px] md:h-[300px]  rounded shadow-2xl"
               />
             </Link>
             <div className=" space-y-10 px-0 md:px-10 max-w-6xl">
-              <h4 className="text-4xl font-semibold text-center">
+              <h4 className="text-lg md:text-2xl font-semibold text-center">
                 <span className="underline decoration-[#F7AB0A]/50">
                   Case of study {i + 1} of {projects.length}:
                 </span>{" "}
                 {project.title}
               </h4>
               <div>
-                <h5 className="text-2xl font-semibold text-center pb-2">
+                <h5 className="text-xl md:text-2xl font-semibold text-center pb-1 md:pb-2">
                   Technologies used:
                 </h5>
                 <div className="flex items-center justify-center space-x-2">
@@ -71,12 +71,12 @@ const Projects = ({ projects }: Props) => {
                       width={50}
                       height={50}
                       alt=""
-                      className="h-10 w-10"
+                      className="h-5 md:h-10 w-5 md:w-10"
                     />
                   ))}
                 </div>
               </div>
-              <p className=" text-lg text-center md:text-left">
+              <p className=" text-xs md:text-lg text-center md:text-left">
                 {project.summary}
               </p>
             </div>
