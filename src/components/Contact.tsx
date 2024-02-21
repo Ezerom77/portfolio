c8ed05ed3ea5ace041ebb05365e774ab3ee74809
ezerom77 <fullstack@ezerom.com.ar>
2024-02-21 18:49:12 -0300

    Contact info updated

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -38,13 +38,13 @@ function Contact({}: Props) {
           </span>
         </h4>
         <div className=" space-y-2">
-          <div className="flex items-center space-x-5 justify-center">
+          {/* <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#F7AB0A] h-4 w-4 md:h-7 md:w-7 animate-pulse" />
             <p className=" text-sm md:text-2xl">+54 9 11 6720 3939</p>
-          </div>
+          </div> */}
           <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#F7AB0A] h-4 w-4 md:h-7 md:w-7 animate-pulse" />
-            <p className=" text-sm md:text-2xl"> Bunos Aires, Argentina</p>
+            <p className=" text-sm md:text-2xl"> Buenos Aires, Argentina</p>
           </div>
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#F7AB0A] h-4 w-4 md:h-7 md:w-7 animate-pulse" />
